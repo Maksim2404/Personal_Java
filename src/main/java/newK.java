7c8b314db75a00707770cc9bf0853e14f81722e0
Maksim Meleshkin <makssamarskiy@icloud.com>
2022-10-17 16:19:45 -0700

    first commit

--- src/main/java/newK.java
+++ src/main/java/newK.java
@@ -1,8 +1,28 @@
 public class newK {
 
-    public static void main(String[] args) {
-        System.out.println("hello");
 
-        System.out.println("Ghbsopfgjsdfjsdsdsgdsg");
+    public static void printFizzBuzz(int n3) {
+
+        for (int i = 0; i <= n3; i++) {
+            if (i % 3==0 && i%5==0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 ==0) {
+                System.out.println("Fizz");
+            } else if (i % 5==0) {
+                System.out.println("Buzz");
+            }else {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        printFizzBuzz(3);
     }
 }
+
